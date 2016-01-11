48235cc8ab467791138eabd35863618684aff8b5
alon1980 <alonvered@gmail.com>
2016-01-11 16:19:08 +0200

    111

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,3 +2,5 @@
 // This is global bootstrap for autoloading
 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
+
+//commenting for sake of phoci - can be deleted
