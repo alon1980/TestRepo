ccae4cf4d26d07f554b0a4ca57991fb40bb8aade
alon1980 <alonvered@gmail.com>
2016-01-11 23:03:56 +0200

    updating phar

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,3 +4,4 @@
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 
 //commenting for sake of phoci - can be deleted
+//commenting for sake of phoci - can be deleted
